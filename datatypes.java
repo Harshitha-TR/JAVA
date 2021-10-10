94043cc28f30f312a386531e5a16c1fab627b87c
Harshitha-TR <92264023+Harshitha-TR@users.noreply.github.com>
2021-10-10 23:46:58 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		int num1=2;
+		short s1 = 3;
+		byte s2 = 1;
+		float num2 = 2.2f;
+		double num3 = 10;
+		char letter = 'a';
+		String stat = "Hai";
+		boolean bool = true;
+		System.out.println(num1);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(letter);
+		System.out.println(stat);
+		System.out.println(bool);
+	}
+}
